a5d9e1d889a040ff8bf456fbb50c4329bf04c2be
Mongy <mongidlanjwa@gmail.com>
2023-08-18 13:46:22 +0200

    front

--- challenge_gui/src/router/index.js
+++ challenge_gui/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+
+
 const routes = [
   {
     path: '/',
@@ -10,10 +12,26 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+   
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/users',
+    name: 'users',
+   
+    component: () => import('../views/UsersView.vue')
+  },
+  {
+    path: '/products',
+    name: 'products',
+   
+    component: () => import('../views/ProductsView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+   
+    component: () => import('../views/ContactView.vue')
   }
 ]
 
---
+++ challenge_gui/src/views/ProductsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Products</h2>
+    </div>
+</template>
+
+<script>
+    export default { 
+    name: 'ProductsView',
+    components: {
+  
+  }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
